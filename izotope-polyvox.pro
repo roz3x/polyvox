6cac03750e83b25722c7938f83c037a78442e201
shivang upadhyay <oroz3x@gmail.com>
2022-04-17 00:08:29 +0530

    playing raw pcm file

--- izotope-polyvox.pro
+++ izotope-polyvox.pro
@@ -8,7 +8,9 @@ CONFIG += c++11
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
+LIBS += -lasound -ltag
 SOURCES += \
+    alsa-play.h \
     main.cpp \
     mainwindow.cpp
 
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,24 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include "alsa-play.h"
 #include <stdlib.h>
+#include <taglib/tag.h>
+#include <taglib/fileref.h>
 
-struct  __wh {
-    char                riff[4];        // RIFF Header      Magic header
-    unsigned long       chunkSize;      // RIFF Chunk Size
-    char                wave[4];        // WAVE Header
-    char                fmt[4];         // FMT header
-    unsigned long       subChunk1Size;  // Size of the fmt chunk
-    unsigned short      audioFormat;    // Audio format 1=PCM,6=mulaw,7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
-    unsigned short      numOfChannel;   // Number of channels 1=Mono 2=Sterio
-    unsigned long       samplesPerSec;  // Sampling Frequency in Hz
-    unsigned long       bytesPerSec;    // bytes per second
-    unsigned short      blockAlign;     // 2=16-bit mono, 4=16-bit stereo
-    unsigned short      bitsPerSample;  // Number of bits per sample
-    char                subChunk2Id[4]; // "data"  string
-    unsigned long       subChunk2Size;  // Sampled data length
-
-} wave_header;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -48,36 +34,8 @@ void MainWindow::open_file_dialog() {
 }
 
 void MainWindow::play_file() {
+    ui->logs->append("playing the file ... \n");
     FILE* f = fopen(filename.toStdString().c_str(), "rb");
-    if (f == 0x00) {
-        ui->logs->append("cant open file "+filename);
-        return;
-    }
-    ui->logs->setText("file is open...\n");
-
-    // parseing the wave header
-    // compute the file size first
-
-    fseek(f, 0, SEEK_END);
-    long filesize = ftell(f);
-    ui->logs->append("file has size: " + QString::number(filesize) + "\n");
-
-    if (filesize < 2048){
-        ui->logs->append("filesize if too low");
-        return;
-    }
-    fseek(f, 0,SEEK_SET);
-    fread(&wave_header, sizeof(wave_header), 1, f);
-    long sizeInAudio = filesize - sizeof(wave_header);
-    long sampleSize = wave_header.bitsPerSample/8;
-
-    ui->logs->append("audio samples :"
-                     +QString::number(sizeInAudio/sampleSize)
-                     + "\n" );
-    ui->logs->append("left off sample :"
-                     +QString::number(sizeInAudio%sampleSize)
-                     + "\n" );
-
-
+    setup_and_play(ui, f, 22500, 2);
     return ;
 }
